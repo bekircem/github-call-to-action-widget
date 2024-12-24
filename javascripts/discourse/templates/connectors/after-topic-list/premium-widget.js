225dbbc9e924e26f6f1aae5f43137bfce70269d7
Bekir Cem Ünalan <unalanbekircem@gmail.com>
2024-12-24 18:47:03 +0300

    a

--- javascripts/discourse/templates/connectors/after-topic-list/premium-widget.js
+++ javascripts/discourse/templates/connectors/after-topic-list/premium-widget.js
@@ -1,8 +1,12 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+
 export default {
   setupComponent(args, component) {
-    component.set(
-      "switchOutletToAfterTopicList",
-      settings.widget_location === "after_topic_list"
-    );
+    withPluginApi("0.8.31", api => {
+      component.set(
+        "switchOutletToAfterTopicList",
+        api.container.lookup("service:theme-settings").get("widget_location") === "after_topic_list"
+      );
+    });
   },
 }; 
--- javascripts/discourse/templates/connectors/before-topic-list/premium-widget.js
+++ javascripts/discourse/templates/connectors/before-topic-list/premium-widget.js
@@ -1,8 +1,12 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+
 export default {
   setupComponent(args, component) {
-    component.set(
-      "switchOutletToBeforeTopicList",
-      settings.widget_location === "before_topic_list"
-    );
+    withPluginApi("0.8.31", api => {
+      component.set(
+        "switchOutletToBeforeTopicList",
+        api.container.lookup("service:theme-settings").get("widget_location") === "before_topic_list"
+      );
+    });
   },
 }; 
